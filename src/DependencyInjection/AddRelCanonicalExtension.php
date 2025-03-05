8ed10464407ace03754de8dd419cd0d26eff721b
Andreas Fieger <fiedsch@ja-eh.at>
2025-03-05 13:11:57 +0100

    Fix #1

--- src/DependencyInjection/AddRelCanonicalExtension.php
+++ src/DependencyInjection/AddRelCanonicalExtension.php
@@ -22,6 +22,23 @@ class AddRelCanonicalExtension extends Extension
             new FileLocator(__DIR__.'/../../config')
         );
         $loader->load('services.yaml');
+
+        if (in_array('ContaoGlossaryBundle', array_keys($container->getParameter('kernel.bundles')), true)) {
+
+            // Add the
+            //
+            //     # unset the original service definition
+            //     Oveleon\ContaoGlossaryBundle\Controller\FrontendModule\GlossaryReaderController: ~
+            //
+            //     # replace the old definition with the new one
+            //     # (the old definition is lost -- which does not matter in our use case)
+            //     Oveleon\ContaoGlossaryBundle\Controller\FrontendModule\GlossaryReaderController:
+            //       class: Codesache\AddRelCanonicalBundle\Controller\FrontendModule\GlossaryReaderController
+            //
+            // part of services.yaml here:
+            $loader->load('glossaryreader.services.yaml');
+
+        }
     }
 
 }
