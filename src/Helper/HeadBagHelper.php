3f96bc1f0cace2597a7a70b6b616bf9a1693a7e6
Andreas Fieger <fiedsch@ja-eh.at>
2025-04-07 12:15:19 +0200

    Be tolerant if canonicalType is not set at all

--- src/Helper/HeadBagHelper.php
+++ src/Helper/HeadBagHelper.php
@@ -33,6 +33,7 @@ class HeadBagHelper
                     $headBag->setCanonicalUri('');
                     break;
                 case DcaHelper::RELCANONICAL_DONOTSET:
+                case '': // if nothing is set (BC)
                     // do not set anything (the rel="canonical" attribute will be set by contao then!
                     break;
                 default:
